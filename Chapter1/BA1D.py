372753034cb617f03102bc8ec7f9e689c6f42365
tlovric <83786267+tlovric@users.noreply.github.com>
2022-08-03 09:43:50 +0200

    Update and rename BA1D.py to Chapter1/BA1D.py

---
+++ Chapter1/BA1D.py
@@ -0,0 +1,19 @@
+#Find All Occurrences of a Pattern in a String
+def patternposition(text, pattern):
+    a = ""
+    for i in range(0, len(text) - len(pattern)+1):
+        if (text[i:(len(pattern) + i)] == pattern):
+            print(text[i])
+            print("usa")
+            a = a + str(i) + " "
+            print (a)
+            print(str(i))
+
+    return a
+
+
+if __name__ == "__main__":
+   text="GATATATGCATATACTT"
+   pattern="ATAT"
+   res = patternposition(text, pattern)
+   print(res)
